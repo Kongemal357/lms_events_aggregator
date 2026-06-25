2fdb33ca2a66788f85a1301a8471d53bbda4a8ba
Kongemal357 <kongemal357@gmail.com>
2026-06-25 16:08:30 +0300

    fix run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@ set -e
 
 export DATABASE_URL="postgresql+asyncpg://${POSTGRES_USERNAME}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DATABASE_NAME}"
 
+source .venv/bin/activate
 python -c "import asyncio; from src.core.database import init_db; asyncio.run(init_db())"
 exec uvicorn src.main:app --host 0.0.0.0 --port 8000

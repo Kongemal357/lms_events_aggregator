6a916f8a3ddd2561ef6674f0720588c04a135ab2
iurii_platonov_1 <kongemal357@gmail.com>
2026-07-01 15:14:38 +0300

    add usecase for get seats

---
+++ src/core/usecases/get_seats.py
@@ -0,0 +1,36 @@
+import uuid
+
+from cachetools import TTLCache
+
+from src.core.clients.events_provider import EventsProviderClient
+from src.core.exeptions import EventNotFound, EventNotPublished
+from src.core.repositories.events import EventRepository
+from src.core.schemas import SeatsResponse
+
+
+class GetSeatsUsecase:
+    """Return available seats for an event, cached for 30 seconds."""
+
+    def __init__(self, client: EventsProviderClient, events_repo: EventRepository):
+        self.client = client
+        self.events_repo = events_repo
+        self.cache: TTLCache = TTLCache(maxsize=1000, ttl=30)
+
+    async def do(self, event_id: uuid.UUID) -> SeatsResponse:
+        event = await self.events_repo.get_by_id(event_id)
+        if event is None:
+            raise EventNotFound()
+        if event.status != "published":
+            raise EventNotPublished()
+
+        cache_key = str(event_id)
+        if cache_key in self.cache:
+            seats = self.cache[cache_key]
+        else:
+            seats = await self.client.get_seats(str(event_id))
+            self.cache[cache_key] = seats
+
+        return SeatsResponse(
+            event_id=event_id,
+            available_seats=seats,
+        )

0eeea57047f74201d7bf3b89f482dab441701ebd
iurii_platonov_1 <kongemal357@gmail.com>
2026-07-02 14:14:49 +0300

    add ticket exceptions

--- src/core/exeptions.py
+++ src/core/exeptions.py
@@ -14,3 +14,27 @@ class SyncInProgress(Exception):
     """Raised when a sync is already running."""
 
     pass
+
+
+class RegistrationDeadlinePassed(Exception):
+    """Raised when trying to register after the registration deadline."""
+
+    pass
+
+
+class SeatUnavailable(Exception):
+    """Raised when the requested seat is already taken."""
+
+    pass
+
+
+class TicketNotFound(Exception):
+    """Raised when a ticket is not found or already cancelled."""
+
+    pass
+
+
+class EventAlreadyPassed(Exception):
+    """Raised when trying to cancel a ticket for a past event."""
+
+    pass

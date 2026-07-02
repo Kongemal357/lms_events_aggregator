97dfe5cb16716be8ea970be8240710fcf599baeb
iurii_platonov_1 <kongemal357@gmail.com>
2026-07-02 09:21:12 +0300

    fix saving cache

--- src/core/usecases/get_seats.py
+++ src/core/usecases/get_seats.py
@@ -4,8 +4,11 @@ from cachetools import TTLCache
 
 from src.core.clients.events_provider import EventsProviderClient
 from src.core.exeptions import EventNotFound, EventNotPublished
+from src.core.logging import logger
 from src.core.repositories.events import EventRepository
 from src.core.schemas import SeatsResponse
+
+_seats_cache: TTLCache = TTLCache(maxsize=1000, ttl=30)
 
 
 class GetSeatsUsecase:
@@ -14,7 +17,6 @@ class GetSeatsUsecase:
     def __init__(self, client: EventsProviderClient, events_repo: EventRepository):
         self.client = client
         self.events_repo = events_repo
-        self.cache: TTLCache = TTLCache(maxsize=1000, ttl=30)
 
     async def do(self, event_id: uuid.UUID) -> SeatsResponse:
         event = await self.events_repo.get_by_id(event_id)
@@ -24,11 +26,11 @@ class GetSeatsUsecase:
             raise EventNotPublished()
 
         cache_key = str(event_id)
-        if cache_key in self.cache:
-            seats = self.cache[cache_key]
+        if cache_key in _seats_cache:
+            seats = _seats_cache[cache_key]
         else:
             seats = await self.client.get_seats(str(event_id))
-            self.cache[cache_key] = seats
+            _seats_cache[cache_key] = seats
 
         return SeatsResponse(
             event_id=event_id,
